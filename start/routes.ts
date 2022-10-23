13d8a777d160d7602895c6749d6b6017a55f9b92
“Pauloviictorss” <paulovictorlvm@gmail.com>
2022-10-23 20:39:20 -0300

    Implementando rotas

--- start/routes.ts
+++ start/routes.ts
@@ -1,25 +1,16 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+Route.resource('/semestres', 'SemestresController').apiOnly()
+Route.resource('/salas', 'SalasController').apiOnly()
+Route.resource('/disciplinas', 'DisciplinasController').apiOnly()
+Route.resource('/cursos', 'CursosController').apiOnly()
+Route.resource('/docentes', 'DocentesController').apiOnly()
+Route.resource('/turmas', 'TurmasController').apiOnly()
+Route.resource('/alunos', 'AlunosController').apiOnly()
+Route.resource('/aulas', 'AulasController').apiOnly()
+Route.resource('/chamadas', 'ChamadasController').apiOnly()
+Route.resource('/turma_alunos', 'TurmaalunosController').apiOnly()
